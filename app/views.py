a34d9ebe780440df2712a7db6b65b9e8ae3a3380
default-007 <brianokola@gmail.com>
2020-04-28 22:19:57 +0300

    Createrview for the news categories

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_articles,get_news_source
 
 # Views
 @app.route('/')
@@ -8,6 +9,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    #getting news_source category
+    abc_news = get_news_source('abc-news')
+    cnn_news = get_news_source('cnn')
+    time_news = get_news_source('time')
+    nbc_news = get_news_source('nbc-news')
+    fox_news = get_news_source('fox-news')
+    espn_news = get_news_source('espn')
+    the_wall_street_journal = get_news_source('the-wall-street-journal')
+    title = 'Home - Welcome to The best News Website Online'
+    return render_template('index.html', title = title, abc = abc_news, cnn = cnn_news, time = time_news , nbc = nbc_news, fox = fox_news, wallstreet = the_wall_street_journal, espn = espn_news)
